c3fcc7e34fe441d2489ee046a7a4c434a6aa165d
LJW <dementor0711@gmail.com>
2025-02-06 15:52:18 +0900

    [2-2] 1월 입출금 내역 합계 계산하기

--- src/main/java/BankTransactionAnalyzerSimple.java
+++ src/main/java/BankTransactionAnalyzerSimple.java
@@ -2,6 +2,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 /**
@@ -18,15 +21,25 @@ import java.util.List;
 public class BankTransactionAnalyzerSimple {
     private static final String RESOURCES = "src/main/resources";
 
-    public static void main(final String... args) throws IOException{
+    public static void main(final String... args) throws IOException {
         final Path path = Paths.get(RESOURCES + "/bank-data-simple.csv");
         final List<String> lines = Files.readAllLines(path);
         double total = 0d;
-        for(final String line:lines){
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        for (final String line : lines) {
             final String[] columns = line.split(",");
-            final double amount = Double.parseDouble(columns[1]);
-            total += amount;
+
+            // 특정 월의 지출 내역
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+            if (date.getMonth() == Month.JANUARY) {
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
+
+//            final double amount = Double.parseDouble(columns[1]);
+//            total += amount;
         }
-        System.out.println("The total for all transaction is "+ total);
+        System.out.println("The total for all transaction is " + total);
     }
 }
